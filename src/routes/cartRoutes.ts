e5a3a8c4dd47eaeca5bcd9b7fd1bbdb51f81582c
Sergionx <sergio180203@gmail.com>
2022-04-18 15:49:36 -0400

    Created API cart route

--- src/routes/cartRoutes.ts
+++ src/routes/cartRoutes.ts
@@ -1,15 +1,21 @@
 import express from "express";
 import {
-  register,
-  authenticate,
-  confirm,
-  forgotPassword,
-  verifyToken,
-  newPassword,
-  profile,
-} from "../controllers/userController";
+  createCart,
+  getCart,
+  addProduct,
+  deleteCart,
+} from "../controllers/cartController";
 import checkAuth from "../middlewares/checkAuth";
 
 const router = express.Router();
+
+router
+  .route("/")
+  .post(checkAuth, createCart) // Crea un nuevo carrito
+  .get(checkAuth, getCart); // Devuelve el carrito del usuario
+router
+  .route("/:id")
+  .put(checkAuth, addProduct) // Agrega un producto al carrito
+  .delete(checkAuth, deleteCart); // Elimina el carrito del usuario
 
 export default router;
