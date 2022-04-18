9bedbe8ca621e09daab6e0f4200f884700d8a959
Sergionx <sergio180203@gmail.com>
2022-04-17 20:27:54 -0400

    .

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,30 @@
-//Libreries
+// Libraries
 import express from "express";
 import morgan from "morgan";
 
-//Routes
+// Routes
 import userRoutes from "./routes/userRoutes";
 import cartRoutes from "./routes/cartRoutes";
 import productRoutes from "./routes/productRoutes";
 import orderRoutes from "./routes/orderRoutes";
 
-
 // Intitializations
 const app = express();
+import conenctarDb from "./database";
+conenctarDb();
 
 // Settings
 app.set("port", process.env.PORT || 3000);
 
 // Middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/products", productRoutes);
-app.use("api/order", orderRoutes)
+app.use("api/order", orderRoutes);
 
 // Satic files
 
