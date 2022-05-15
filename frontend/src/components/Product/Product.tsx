fcb00af8f9a4038fc6838fb034f97783b6b8d1d8
Sergionx <sergio180203@gmail.com>
2022-05-15 17:30:15 -0400

    Now ProductCard is responsible for showing the product in the shop

--- frontend/src/components/Product/Product.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import useProduct from "../../hooks/useProduct";
-
-function Product() {
-  const { products } = useProduct();
-  return <div>Product</div>;
-}
-
-export default Product;
---
+++ frontend/src/components/Product/ProductCard.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import IProduct from "../../../../backend/models/interfaces/IProduct";
+import useProduct from "../../hooks/useProduct";
+
+interface IPropsProduct {
+  product: IProduct;
+}
+
+function ProductCard(props: IPropsProduct) {
+  const navigate = useNavigate();
+
+  function handleImageClick() {
+    navigate(props.product.title);
+  }
+
+  return (
+    <>
+      <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+        <img
+          src={props.product.imageUrl}
+          className="w-full h-full object-center object-cover lg:w-full lg:h-full 
+            hover:cursor-pointer"
+          onClick={handleImageClick}
+        />
+      </div>
+      <div className="mt-4 flex justify-between">
+        <div>
+          <h3 className="text-sm text-gray-700">
+            <Link to={props.product.title} className="">
+              {props.product.title}
+            </Link>
+          </h3>
+        </div>
+        <p className="text-sm font-medium text-gray-900">
+          {props.product.price} $
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default ProductCard;
