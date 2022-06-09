1733783592ce7a04ae3c5a37d682c967e7acd2f3
Sergionx <sergio180203@gmail.com>
2022-06-08 20:18:49 -0400

    Improveded readbility of error message

--- backend/middlewares/checkAuth.ts
+++ backend/middlewares/checkAuth.ts
@@ -14,20 +14,16 @@ async function checkAuth(req: Request, res: Response, next: NextFunction) {
     try {
       const token = req.headers.authorization.split(" ")[1];
       const decoded: any = jwt.verify(token, process.env.JWT_SECRET as string);
-
       req.usuario = await Usuario.findById(decoded.id).select(
         "-password -confirmado -token -confirmed  -createdAt -updatedAt -__v"
       );
-
       return next();
     } catch (error) {
-      return res.status(404).json({ msg: "Hubo un error" });
+      return res.status(404).json({ msg: "No se pudo verificar su token" });
     }
   } else {
     return res.status(401).json({ msg: "No contiene autorización" });
   }
-
-  next();
 }
 
 export default checkAuth;
