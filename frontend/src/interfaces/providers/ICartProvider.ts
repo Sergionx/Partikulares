e78b65efaeda1f0d11efa17af77e57f1e076136c
Sergionx <sergio180203@gmail.com>
2022-06-09 20:43:34 -0400

    addProduct

--- frontend/src/interfaces/providers/ICartProvider.ts
+++ frontend/src/interfaces/providers/ICartProvider.ts
@@ -1,9 +1,11 @@
 import ICart from "../../../../backend/models/interfaces/ICart";
+import IProduct from "../../../../backend/models/interfaces/IProduct";
 
 
 interface ICartProvider {
   setCart: React.Dispatch<React.SetStateAction<ICart>>
   cart: ICart
+  addProduct(product: IProduct): void
 }
 
 export default ICartProvider;
