727a0dc2f986b5c94591f6c3e5c7b9db515ac502
Sergionx <sergio180203@gmail.com>
2022-07-18 23:57:51 -0400

    ugly UI for testing

--- frontend/src/pages/compra/Compra.tsx
+++ frontend/src/pages/compra/Compra.tsx
@@ -1,7 +1,10 @@
+//TODO - Make it impossible to access this page if no token is present
+//FIXME - Si modifico un producto, no se refleja aca
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import IProduct from "../../../../backend/models/interfaces/IProduct";
 import CartProduct from "../../components/Cart/CartProduct";
+import axiosClient from "../../config/axiosClient";
 
 interface IState {
   products: { product: IProduct; quantity: number }[];
@@ -13,7 +16,26 @@ function Compra() {
   const state = location.state as IState;
   const { products, totalPrice } = state;
 
-//TODO- Utlizar una carta para los productos que no se puedan borrar
+  async function handleBuyBtn(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    console.log("Compra realizada");
+  }
+
+  async function handlePaypalBtn(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    const token = localStorage.getItem("token");
+    const config = {
+      headers: {
+        Content0type: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const {data} = await axiosClient.post(`/orders/paypal`, {products}, config);
+    console.log(data)
+    window.location.href = data.links[1].href;
+
+  }
+  //TODO- Utlizar una carta para los productos que no se puedan borrar
   return (
     <>
       <div className="flex flex-row">
@@ -30,7 +52,18 @@ function Compra() {
           </div>
         </ul>
         <div className="basis-1/2">
-
+          <button
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleBuyBtn}
+          >
+            COMPRAR YA
+          </button>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handlePaypalBtn}
+          >
+            PAYPAL
+          </button>
         </div>
       </div>
     </>
